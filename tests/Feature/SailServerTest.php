333aed61a2ae7f813c79df1163f2a249dc4b03cb
my4fdal <my4fdal@gmail.com>
2022-03-10 08:38:43 +0700

    create curl badaso project

---
+++ tests/Feature/SailServerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class SailServerTest extends TestCase
+{
+    public function test_the_homepage_redirects_to_the_laravel_docs()
+    {
+        $this->get('/')->assertRedirect('https://laravel.com/docs');
+    }
+
+    public function test_it_can_return_the_sail_install_script()
+    {
+        $response = $this->get('/example-app');
+
+        $response->assertStatus(200);
+        $response->assertSee("laravelsail/php81-composer:latest");
+        $response->assertSee('bash -c "laravel new example-app && cd example-app && php ./artisan sail:install --with=mysql,redis,meilisearch,mailhog,selenium "', false);
+    }
+
+    public function test_different_services_can_be_picked()
+    {
+        $response = $this->get('/example-app?with=postgresql,redis,selenium');
+
+        $response->assertStatus(200);
+        $response->assertSee('php ./artisan sail:install --with=postgresql,redis,selenium');
+    }
+
+    public function test_it_adds_the_devcontainer_upon_request()
+    {
+        $response = $this->get('/example-app?with=postgres&devcontainer');
+
+        $response->assertStatus(200);
+        $response->assertSee('bash -c "laravel new example-app && cd example-app && php ./artisan sail:install --with=postgres --devcontainer"', false);
+    }
+
+    public function test_it_does_not_accepts_domains_with_a_dot()
+    {
+        $response = $this->get('/foo.test');
+
+        $response->assertStatus(400);
+        $response->assertSee('Invalid site name. Please only use alpha-numeric characters, dashes, and underscores.');
+    }
+}
