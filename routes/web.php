ad3b8f437845200d8294a7268a45983091292277
my4fdal <my4fdal@gmail.com>
2022-03-10 09:50:46 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\ValidationException;
 
-Route::redirect('/', 'https://laravel.com/docs', 302);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::get('/{name}', function (Request $request, $name) {
-    try {
-        Validator::validate(['name' => $name], ['name' => 'string|alpha_dash']);
-    } catch (ValidationException $e) {
-        return response('Invalid site name. Please only use alpha-numeric characters, dashes, and underscores.', 400);
-    }
-
-    $php = $request->query('php', '81');
-
-    $services = $request->query('with', 'mysql,redis,meilisearch,mailhog,selenium');
-
-    $devcontainer = $request->has('devcontainer') ? '--devcontainer' : '';
-
-    $script = str_replace(
-        ['{{ php }}', '{{ name }}', '{{ services }}', '{{ devcontainer }}'],
-        [$php, $name, $services, $devcontainer],
-        file_get_contents(resource_path('scripts/php.sh'))
-    );
-
-    return response($script, 200, ['Content-Type' => 'text/plain']);
+Route::get('/', function () {
+    return view('welcome');
 });
