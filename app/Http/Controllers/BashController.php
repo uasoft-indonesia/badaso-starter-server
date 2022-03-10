a369990488cc0acd06427151869a6a5c392c30a6
my4fdal <my4fdal@gmail.com>
2022-03-10 11:31:58 +0700

    update bash command

--- app/Http/Controllers/BashController.php
+++ app/Http/Controllers/BashController.php
@@ -6,10 +6,34 @@ use Illuminate\Http\Request;
 
 class BashController extends Controller
 {
-    public function starterZip(){
+    public function starterZip()
+    {
         $path_stater_zip = base_path(".badaso-starter/starter.zip");
         return response(file_get_contents($path_stater_zip), 200, [
             'Content-Type' => 'application/zip',
+        ]);
+    }
+
+    public function createProject(Request $request, $project_name)
+    {
+        $laravel_version = $request->get('laravel-version', '8.0');
+        $badaso_version = $request->get('badaso-version', '2.0');
+
+        $bash = file_get_contents(base_path(".badaso-starter/starter.stub"));
+        $bash = str_replace([
+            "{{laravel-version}}",
+            "{{project-name}}",
+            "{{badaso-version}}",
+            "{{app_url}}"
+        ], [
+            $laravel_version,
+            $project_name,
+            $badaso_version,
+            env('APP_URL', 'http://localhost:8000')
+        ], $bash);
+
+        return response($bash, 200, [
+            'Content-Type' => 'text/plain'
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/starter.zip', [BashController::class, 'starterZip']);
+Route::get('/{project_name}', [BashController::class, 'createProject']);
